18eee8b1ee026084870c1f13551f6d64b24ce173
xbpeng121 <pengxianbin@szbit.cn>
2025-07-02 17:01:04 +0800

    修改版本号

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.15.6-alpha.1",
+        default="0.15.6-sibat-alpha.1",
     )
 
     COMMIT_SHA: str = Field(
